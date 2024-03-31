5719d0a6cda60e48a1dcb127854dbf75d1126a3c
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-03-31 16:25:21 +0000

    auth

--- lib/twilio.ts
+++ lib/twilio.ts
@@ -1,6 +1,6 @@
 import twilio from 'twilio';
 
-const accountSid = 'ACd01eed3e4c1fe985511e39a40749a8c0';
+const accountSid = process.env.TWILIO_SID;
 const authToken = process.env.TWILIO_TOKEN; // secret key
 export const verifySid = 'VAebe1a38ec48b7b0541cea274c75392e9';
 
