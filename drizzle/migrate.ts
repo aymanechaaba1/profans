b6ad7a92c7e5b8fb1317e2c93251662d40372f08
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-04-01 12:25:20 +0000

    auth

--- drizzle/migrate.ts
+++ drizzle/migrate.ts
@@ -2,7 +2,7 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import { migrate } from 'drizzle-orm/neon-http/migrator';
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(process.env.POSTGRES_URL!);
 const db = drizzle(sql);
 
 const main = async () => {
--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -4,21 +4,30 @@ import {
   pgTable,
   text,
   timestamp,
+  uniqueIndex,
   uuid,
 } from 'drizzle-orm/pg-core';
 
 export const genderEnum = pgEnum('gender', ['male', 'female']);
 
-export const users = pgTable('users', {
-  id: uuid('id').primaryKey().defaultRandom(),
-  createdAt: timestamp('created_at').defaultNow(),
-  updatedAt: timestamp('updated_at').defaultNow(),
-  gender: genderEnum('gender').notNull(),
-  firstname: text('firstname').notNull(),
-  lastname: text('lastname').notNull(),
-  cin: text('cin').unique().notNull(),
-  birthdate: timestamp('birthdate').notNull(),
-  city: text('city').notNull(),
-  phone: text('phone').unique().notNull(),
-  email: text('email').unique().notNull(),
-});
+export const users = pgTable(
+  'users',
+  {
+    id: uuid('id').primaryKey().defaultRandom(),
+    createdAt: timestamp('created_at').defaultNow(),
+    updatedAt: timestamp('updated_at').defaultNow(),
+    gender: genderEnum('gender').notNull(),
+    firstname: text('firstname').notNull(),
+    lastname: text('lastname').notNull(),
+    cin: text('cin').unique().notNull(),
+    birthdate: timestamp('birthdate').notNull(),
+    city: text('city').notNull(),
+    phone: text('phone').unique().notNull(),
+    email: text('email').unique().notNull(),
+  },
+  (users) => {
+    return {
+      uniqueIdx: uniqueIndex('unique_idx').on(users.email),
+    };
+  }
+);
