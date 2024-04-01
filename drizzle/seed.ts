ca02df4894d44f70a8bc0c0e5112c32d87fa6d7a
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-04-01 11:20:15 +0000

    auth

--- drizzle/seed.ts
+++ drizzle/seed.ts
@@ -2,9 +2,7 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
-export let sql: any;
-
-if (process.env.DATABASE_URL) sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.DATABASE_URL!);
 const db = drizzle(sql, { schema });
 
 export default db;
