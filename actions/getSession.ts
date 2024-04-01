8a348e9662751d255247717efc4f1b410b2adb1d
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-04-01 12:45:03 +0000

    auth

--- actions/getSession.ts
+++ actions/getSession.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import db, { sql } from '@/drizzle/seed';
+import db from '@/drizzle/seed';
 import { validJWT } from '@/middleware';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
--- drizzle/seed.ts
+++ drizzle/seed.ts
@@ -1,8 +1,7 @@
-import { neon } from '@neondatabase/serverless';
+import { sql } from '@vercel/postgres';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import * as schema from './schema';
 
-export const sql = neon(process.env.POSTGRES_URL!);
 const db = drizzle(sql, { schema });
 
 export default db;
