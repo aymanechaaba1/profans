dff15f65b3f780401ee66faec37dcb4e98779f91
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-04-02 13:15:50 +0000

    auth

--- actions/getSession.ts
+++ actions/getSession.ts
@@ -4,6 +4,7 @@ import db from '@/drizzle/seed';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { validJWT } from './validJWT';
+import { sql } from '@vercel/postgres';
 
 export async function getSession() {
   const token = cookies().get('jwt')?.value;
@@ -12,9 +13,7 @@ export async function getSession() {
     const payload = await validJWT(token!);
     if (!payload) return redirect('/login');
 
-    const user = await db.query.users.findFirst({
-      where: (users, { eq }) => eq(users.id, payload.id),
-    });
+    const user = await sql`SELECT * FROM users WHERE users.id = ${payload.id}`;
 
     return { user };
   } catch (err) {
