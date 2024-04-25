7a186367ef0a9b53c23b2ee17215ed39d1ff5ee9
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-04-25 12:42:37 +0100

    download tickets from orders history

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -141,7 +141,6 @@ export async function POST(req: NextRequest) {
         await db.delete(cartItems).where(eq(cartItems.cartId, user.cart.id));
 
         revalidatePath('/', 'layout');
-        revalidateTag('user');
       } catch (err) {
         console.log(err);
       }
