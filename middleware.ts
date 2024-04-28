9df689a64296914ccbdee0d3815de560677b9684
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-04-28 15:56:29 +0100

    beautiful emails

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,6 @@ import type { NextRequest } from 'next/server';
 import { verifyJWT } from './actions/verifyJWT';
 import { Ratelimit } from '@upstash/ratelimit';
 import { kv } from '@vercel/kv';
-import db from './drizzle';
-import { cookies } from 'next/headers';
 
 const authRoutes = ['/login', '/register'];
 const publicRoutes = ['/', '/events'];
@@ -16,25 +14,19 @@ const ratelimit = new Ratelimit({
 });
 
 export async function middleware(request: NextRequest) {
-  // if (request.method === 'POST') {
-  //   const ip = request.ip ?? '127.0.0.1';
-  //   const { success, pending, limit, reset, remaining } = await ratelimit.limit(
-  //     ip
-  //   );
-  //   console.log(success, remaining);
-  //   if (!remaining) return null;
-  // }
+  if (request.method === 'POST') {
+    const ip = request.ip ?? '127.0.0.1';
+    const { success, pending, limit, reset, remaining } = await ratelimit.limit(
+      ip
+    );
+    if (!remaining) return null;
+  }
 
   const url = request.nextUrl;
   const token = request.cookies.get('jwt')?.value;
   const payload = await verifyJWT(token!);
-  if (!payload) return null;
 
-  const user = await db.query.users.findFirst({
-    where: (users, { eq }) => eq(users.id, payload.id),
-  });
-
-  const loggedIn = !!token && !!payload && !!user;
+  const loggedIn = !!token && !!payload;
 
   let isApiRoute = url.pathname.startsWith('/api');
   let isAuthRoute = authRoutes.includes(url.pathname);
