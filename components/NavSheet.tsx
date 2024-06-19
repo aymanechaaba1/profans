8913d56b1657c273e38da425c451d5c6ceef5ac1
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-06-19 19:44:31 +0100

    light mode

--- components/NavSheet.tsx
+++ components/NavSheet.tsx
@@ -25,9 +25,9 @@ function NavSheet() {
           onClick={() => setIsOpen(false)}
           href={`/events`}
           className={cn(
-            'flex items-center gap-x-5 w-full py-2 px-5 hover:bg-slate-900 text-gray-100',
+            'flex items-center gap-x-5 w-full py-2 px-5 dark:hover:bg-slate-900 hover:bg-zinc-200',
             {
-              'bg-slate-900': pathname === '/events',
+              'dark:bg-slate-900 bg-zinc-200': pathname === '/events',
             }
           )}
         >
